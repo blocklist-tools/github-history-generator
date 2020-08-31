7d4daa376d973012dc849a3aa33055b4a9410a33
Daniel <daniel@developerdan.com>
2020-08-31 16:21:42 +0000

    Adding file hash checking

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 docker-compose run --rm run --username lightswitch05 --repo hosts "ads-and-tracking-extended.txt" "*ads-and-tracking.txt"
 docker-compose run --rm run --username StevenBlack --repo hosts "*/StevenBlack/hosts"
 docker-compose run --rm run --username anudeepND --repo blacklist "adservers.txt"
+docker-compose run --rm run --username dead-hosts --repo domain_blocklist_web_dbl-oisd-nl "origin.list"
 ```
--- run.py
+++ run.py
@@ -48,6 +48,12 @@ def find_matching_file(patterns: list) -> Union[None, str]:
     return None
 
 
+def get_file_hash(file: str) -> str:
+    p = subprocess.Popen(['sha256sum', f'.{file}'], cwd=CLONE_PATH, stdout=subprocess.PIPE)
+    output = p.communicate()[0].decode('utf-8').strip()
+    return output.split()[0]
+
+
 def get_commit_details() -> dict:
     p = subprocess.Popen(['git', 'rev-parse', 'HEAD'], cwd=CLONE_PATH, stdout=subprocess.PIPE)
     commit_hash = p.communicate()[0].decode('utf-8').strip()
@@ -80,14 +86,25 @@ def main():
     clone_repo(args.username, args.repo)
     files = list()
     more_history = True
+    previous_hash = None
     while more_history:
         details = get_commit_details()
         file = find_matching_file(args.patterns)
         if file:
-            files.append({
-                'url': build_github_link(args.username, args.repo, details['commit_hash'], file),
-                'commit_epoch': details['commit_epoch']
-            })
+            new_hash = get_file_hash(file)
+            if previous_hash == new_hash:
+                print(f'INFO: hash matches of previous entry')
+                files[-1] = {
+                    'url': build_github_link(args.username, args.repo, details['commit_hash'], file),
+                    'commit_epoch': details['commit_epoch']
+                }
+            else:
+                print(f'INFO: New file hash: ${new_hash}')
+                files.append({
+                    'url': build_github_link(args.username, args.repo, details['commit_hash'], file),
+                    'commit_epoch': details['commit_epoch']
+                })
+            previous_hash = new_hash
         else:
             print(f'WARNING: No match found for commit {details["commit_hash"]}')
         more_history = walk_repo()
